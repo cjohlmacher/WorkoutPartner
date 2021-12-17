331de3e7d88d033d17576b3dd6a756661b8bf145
Chris Ohlmacher <55671489+cjohlmacher@users.noreply.github.com>
2021-12-16 16:59:14 -0800

    responsive design for activities

--- seed.py
+++ seed.py
@@ -10,9 +10,9 @@ def batch_insert(entry_list):
 db.drop_all()
 db.create_all()
 
-user_1 = User(username="pieguy52",email="pieguy52@gmail.com",password="password")
-user_2 = User(username="donutdude52",email="donutdude52@gmail.com",password="password")
-user_3 = User(username="cakecomrade52",email="cakecomrade52@gmail.com",password="password")
+user_1 = User(username="nightcrawler52",email="nightcrawler52@gmail.com",password="password")
+user_2 = User(username="donutdude11",email="donutdude52@gmail.com",password="password")
+user_3 = User(username="amorfeo89",email="amorfeo89@gmail.com",password="password")
 
 batch_insert([user_1,user_2,user_3])
 
@@ -45,8 +45,7 @@ workout_3 = Workout(creator=2)
 batch_insert([workout_1,workout_2,workout_3])
 
 workout_activity_1 = Workout_Activity(workout_id=1,activity_id=1)
-workout_activity_2 = Workout_Activity(workout_id=2,activity_id=3)
-workout_activity_3 = Workout_Activity(workout_id=3,activity_id=3)
-workout_activity_4 = Workout_Activity(workout_id=3,activity_id=2)
+workout_activity_2 = Workout_Activity(workout_id=3,activity_id=3)
+workout_activity_3 = Workout_Activity(workout_id=3,activity_id=2)
 
-batch_insert([workout_activity_1,workout_activity_2,workout_activity_3,workout_activity_4])
+batch_insert([workout_activity_1,workout_activity_2,workout_activity_3])
