7abf80f4f74585a690f63ed923514e5a2ae544e2
Chris Ohlmacher <55671489+cjohlmacher@users.noreply.github.com>
2021-12-16 16:12:44 -0800

    fix heroku postgres issue

--- app.py
+++ app.py
@@ -17,8 +17,10 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///workoutcompanion'))
+uri = os.environ.get("DATABASE_URL", 'postgresql:///workoutcompanion')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
